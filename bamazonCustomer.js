d2be2a2773025d0f0d81ce5fc029666e5895792f
GitDarren <darren321@gmail.com>
2018-01-30 10:10:28 -0600

    Created database and inputted products into the database.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,6 +5,33 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "Bamazon"
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the ????? function after the connection is made to prompt the user
+    queryAllProducts();
+  });
 
-/////////Let's psuedocode//////////////
+/////Create a start function the shows all of the products with associated information in the command line//////
+  function queryAllProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price+ " | " + res[i].stock_quantity);
+      }
+      console.log("-----------------------------------");
+    });
+  }
 
