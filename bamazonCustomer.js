f4c56e84bab8016705811b15b29e5ac26b0d9493
GitDarren <darren321@gmail.com>
2018-01-30 08:05:11 -0600

    Setting up initial files for Bamazon App

---
+++ bamazonCustomer.js
@@ -0,0 +1,10 @@
+
+
+/////////Amazon-like CLI Application using Node.js and MySQL///////////////////
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+/////////Let's psuedocode//////////////
+
